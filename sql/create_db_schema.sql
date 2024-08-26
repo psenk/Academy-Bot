7e752eab969fa1d2b0f4a07a745725f2f482ecf1
Tyler <psenk168@gmail.com>
2024-08-25 23:28:12 -0500

    schema updated with more voting period info

--- sql/create_db_schema.sql
+++ sql/create_db_schema.sql
@@ -1,16 +1,26 @@
-DROP TABLE IF EXISTS current_votes;
-DROP TABLE IF EXISTS past_votes;
+DROP TABLE IF EXISTS active_voting_periods;
+DROP TABLE IF EXISTS past_voting_periods;
 
-CREATE TABLE current_votes (
+CREATE TABLE active_voting_periods (
     id SERIAL PRIMARY KEY,
+    title VARCHAR(500) NOT NULL,
     nominee VARCHAR(100),
+    comments VARCHAR(1000),
     members VARCHAR(100)[] NOT NULL,
-    votes JSONB[] NOT NULL
+	voted VARCHAR(100)[],
+    votes JSONB[],
+	start_date TIMESTAMP NOT NULL,
+	length INT NOT NULL
 );
 
-CREATE TABLE past_votes (
+CREATE TABLE past_voting_periods (
     id SERIAL PRIMARY KEY,
+    title VARCHAR(500),
     nominee VARCHAR(100),
+    comments VARCHAR(1000),
     members VARCHAR(100)[] NOT NULL,
-    votes JSONB NOT NULL
+	voted VARCHAR(100)[],
+    votes JSONB NOT NULL,
+	start_date TIMESTAMP NOT NULL,
+	length INT NOT NULL
 );
